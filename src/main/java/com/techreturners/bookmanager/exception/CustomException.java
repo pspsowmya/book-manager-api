d5035829919bf5ee3f556dfff477c6f4068b0d1e
Ben Donaghy <bendonaghy321@gmail.com>
2022-02-17 23:15:37 +0000

    constructor and getters for exception details class

--- src/main/java/com/techreturners/bookmanager/exception/CustomException.java
+++ src/main/java/com/techreturners/bookmanager/exception/CustomException.java
@@ -6,11 +6,29 @@ import java.time.ZonedDateTime;
 
 public class CustomException {
     private ZonedDateTime timeStamp;
-    private String message;
     private Throwable throwable;
     private HttpStatus HttpStatus;
+    private String message;
 
+    public CustomException(ZonedDateTime timeStamp, Throwable throwable,
+                           org.springframework.http.HttpStatus httpStatus, String message) {
+        this.timeStamp = timeStamp;
+        this.throwable = throwable;
+        HttpStatus = httpStatus;
+        this.message = message;
+    }
 
+    public ZonedDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
+    }
+
+    public org.springframework.http.HttpStatus getHttpStatus() {
+        return HttpStatus;
+    }
 
     public String getMessage() {
         return message;
--- src/main/java/com/techreturners/bookmanager/exception/ExceptionHandler.java
+++ src/main/java/com/techreturners/bookmanager/exception/ExceptionHandler.java
@@ -1,6 +1,11 @@
 package com.techreturners.bookmanager.exception;
 
+import org.springframework.http.ResponseEntity;
+
 public class ExceptionHandler {
 
+    public ResponseEntity<Object> handleGetEmptyException(GetEmptyException e) {
+
+    }
 
 }
